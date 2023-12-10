aeab6a1a6775ced9bf5d262c93b507be8698fe5c
Sydney Escobar <sydney_1165@yahoo.com>
2023-12-10 22:51:28 +0000

    Furniture-Website (Polishing up)

--- furniture-website/dynamic-server.py
+++ furniture-website/dynamic-server.py
@@ -65,11 +65,10 @@ def delete_t():
     connection = mysql.connector.connect(**creds)
     mycursor = connection.cursor()
     ln= request.args.get('ln')
-    print(ln)
     mycursor.execute("SELECT InvoiceID FROM LineItem WHERE LineNumber= %s", (ln,))
     invoiceID = mycursor.fetchone()
     print(invoiceID)
-    print('gets here1')
+
     if invoiceID is not None:
         # Check if the invoice has any remaining line items
         mycursor.execute("SELECT COUNT(*) FROM LineItem WHERE InvoiceID = %s", (invoiceID[0],))
@@ -89,17 +88,22 @@ def delete_t():
 
 # Edits the transaction related to the Item ID and Line Number 
 @app.route('/edit-transact.html', methods=['GET'])
-def edit_t():
+def edit_tran():
     connection = mysql.connector.connect(**creds)
     mycursor = connection.cursor()
-    id= request.form.get('itemid')
-    if id is not None:
-        mycursor.execute("SELECT * FROM LineItem WHERE LineNumber= %s", (id,))
-        connection.commit()
+    ln = request.args.get('ln')
+    mycursor.execute('SELECT ID, DateTime from Invoice;')
+    invoice = mycursor.fetchall()
+    mycursor.execute('SELECT ItemID, FurnitureType from Furniture;')
+    furniture = mycursor.fetchall()
+    mycursor.execute('SELECT max(ItemID) from Furniture;')
+    max= mycursor.fetchall()
+    info = [invoice,furniture,max,ln]
     mycursor.close()
     connection.close()
-    return redirect('../')
+    return render_template('edit-transact.html', myresult=info)
 
+# Adds transactions
 @app.route('/add-transact.html')
 def add_transact():
     connection = mysql.connector.connect(**creds)
@@ -114,11 +118,12 @@ def add_transact():
     connection.close()
     return render_template('add-transact.html', myresult=[invoice,furniture,max])
 
-
+# Renders the page to edit furniture forms
 @app.route('/edit-furniture.html')
 def edit_furn():
     return render_template('edit-furniture.html')
 
+# Will add to furniture
 @app.route('/add', methods=['POST'])
 def add():
     connection = mysql.connector.connect(**creds)
@@ -136,6 +141,7 @@ def add():
     connection.close()
     return redirect('../')
 
+# Will delete from furniture
 @app.route('/delete', methods=['POST'])
 def delete_f():
     connection = mysql.connector.connect(**creds)
@@ -148,6 +154,7 @@ def delete_f():
     connection.close()
     return redirect('../')
 
+# WIll update a row in Furniture
 @app.route('/update', methods=['POST'])
 def update():
     connection = mysql.connector.connect(**creds)
@@ -166,24 +173,43 @@ def update():
     connection.close()
     return redirect('../')
 
-@app.route('/edit-transact.html', methods = ['GET'])
-def edit_trans():
+
+# Updates the transactions from the edit-transact page
+@app.route('/edit_t', methods = ['POST'])
+def edit_t():
     connection = mysql.connector.connect(**creds)
     mycursor = connection.cursor()
-    ln =  request.args.get('ln')
+    ln = request.form.get('ln')
+    itemID = request.form.get('itemID')
+    invoiceID = request.form.get('invoiceID')
+    quant = request.form.get('quant')
+    price = request.form.get('price')
+    print(1)
+    # Checks if a new invoice needs to be created
+    mycursor.execute('Select count(*) From Invoice where ID = %s', (invoiceID,))
+    if mycursor.fetchone()[0] == 0:
+        # Would like to add ClientID as well but might be too complicated for now
+        mycursor.execute('Insert into Invoice (ID) values (%s)', (invoiceID,))
+        connection.commit()
+    mycursor.execute('''UPDATE LineItem SET 
+                     ItemID = %s,
+                     InvoiceID = %s,
+                     Quantity = %s,
+                     Price = %s
+                     WHERE LineNumber = %s;
+                     ''', (itemID,invoiceID,quant,price,ln))
+    print(2)
+    # Checks if there is a corresponding row in LineItem for THis particular invoice
+    mycursor.execute("SELECT COUNT(*) FROM LineItem WHERE InvoiceID = %s", (invoiceID,))
+    if mycursor.fetchone()[0] == 0:
+        mycursor.execute('DELETE FROM Invoice WHERE ID=%s',(invoiceID,))
+    print(3)
+    connection.commit()
     mycursor.close()
     connection.close()
-    return render_template('edit-transact.html', ln=ln)
+    return redirect('../all_transact')
 
-@app.route('/update_t')
-def update_t():
-    connection = mysql.connector.connect(**creds)
-    mycursor = connection.cursor()
-
-    mycursor.close()
-    connection.close()
-    return redirect('../transact.html')
-
+# Adds the transaction from the add-transact page
 @app.route('/add_t', methods=['POST'])
 def add_t():
     connection = mysql.connector.connect(**creds)
@@ -193,7 +219,7 @@ def add_t():
     quant = request.form.get('quant')
     price = request.form.get('price')
     mycursor.execute('Select count(*) From Invoice where ID = %s', (invoiceID,))
-    if mycursor.fetchall()[0][0] == 0:
+    if mycursor.fetchone()[0] == 0:
         # Would like to add ClientID as well but might be too complicated for now
         mycursor.execute('Insert into Invoice (ID) values (%s)', (invoiceID,))
     mycursor.execute('Insert INTO LineItem (InvoiceID, ItemID, Quantity, Price) values (%s,%s,%s,%s)',(invoiceID,itemID,quant, price))
@@ -202,25 +228,72 @@ def add_t():
     connection.close()
     return redirect('../all_transact')
 
-
-
+# Will bring up the forms page needed to edit details
 @app.route('/edit-details.html', methods=['GET'])
 def edit_det():
     connection = mysql.connector.connect(**creds)
     mycursor = connection.cursor()
     itemID = request.args.get('itemID')
-    mycursor.execute('Select Color from Color;')
+    mycursor.execute('''Select DISTINCT Color from Color 
+                     WHERE Color NOT IN (
+                        SELECT C.Color from Color C 
+                        join Furniture F on C.ItemID = F.ItemID 
+                        where C.ItemID = %s)''', (itemID,))
     colors = mycursor.fetchall()
-    mycursor.execute('Select Material from Material;')
+    mycursor.execute('''Select DISTINCT Material from Material 
+                     WHERE Material NOT IN (
+                        SELECT M.Material from Material M 
+                        join Furniture F on M.ItemID = F.ItemID 
+                        where M.ItemID = %s)''', (itemID,))
     materials = mycursor.fetchall()
-    mycursor.execute('SELECT C.Color from Color C join Furniture F on C.ItemID = F.ItemID where C.ItemID = %s', (itemID,))
+    mycursor.execute('SELECT DISTINCT C.Color from Color C join Furniture F on C.ItemID = F.ItemID where C.ItemID = %s', (itemID,))
     cur_col = mycursor.fetchall()
-    mycursor.execute('SELECT M.Material from Material M join Furniture F on M.ItemID = F.ItemID where M.ItemID = %s', (itemID,))
+    mycursor.execute('SELECT DISTINCT M.Material from Material M join Furniture F on M.ItemID = F.ItemID where M.ItemID = %s', (itemID,))
     cur_mat = mycursor.fetchall()
     info = [itemID,colors,materials,cur_col,cur_mat]
     mycursor.close()
     connection.close()
     return render_template('edit-details.html',info = info)
+
+@app.route('/add_d', methods=['POST'])
+def add_d():
+    connection = mysql.connector.connect(**creds)
+    mycursor = connection.cursor()
+    itemID = request.form.get('itemID')
+    colors = request.form.getlist('colors')
+    mats = request.form.getlist('materials')
+    
+    if colors != []:
+        for c in colors:
+            print(c)
+            mycursor.execute('Insert INTO Color (Color, ItemID) values (%s,%s);',(c,itemID))
+    if mats != []:
+        for m in mats:
+            mycursor.execute('Insert INTO Material (Material,ItemID) values (%s,%s)', (m,itemID))
+    connection.commit()
+    mycursor.close()
+    connection.close()
+    return redirect(f'../details.html?id={itemID}')
+
+@app.route('/delete_d', methods=['POST'])
+def del_d():
+    connection = mysql.connector.connect(**creds)
+    mycursor = connection.cursor()
+    itemID = request.form.get('itemID')
+    colors = request.form.getlist('c-colors')
+    mats = request.form.getlist('c-materials')
+    if colors != []:
+        for c in colors:
+            print(c)
+            mycursor.execute('Delete FROM Color Where Color =%s and ItemID=%s',(c, itemID))
+            connection.commit()
+    if mats != []:
+        for m in mats:
+            mycursor.execute('Delete FROM Material Where Material =%s and ItemID=%s',(m, itemID))
+            connection.commit()
+    mycursor.close()
+    connection.close()
+    return redirect(f'../details.html?id={itemID}')
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True, host="0.0.0.0")
